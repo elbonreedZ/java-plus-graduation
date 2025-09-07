f70a66eb287d7e9f1c5520ac56804ee33c763931
Елена <elbonreed05@yandex.ru>
2025-09-08 01:19:27 +0300

    Поправила

--- main-service/src/main/java/ru/practicum/MainServiceApplication.java
+++ main-service/src/main/java/ru/practicum/MainServiceApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication(scanBasePackages = {"ru.practicum.client"})
-@EnableFeignClients(basePackages = "ru.practicum.client")
+@SpringBootApplication
+@EnableFeignClients
 public class MainServiceApplication {
     public static void main(String[] args) {
             SpringApplication.run(MainServiceApplication.class, args);
